aff5064fd9e53958c7eb7aa127095725de6d6030
Brian Hambleton <bhambl3t0n1@gmail.com>
2019-11-05 16:32:58 -0800

    Got 10 mil entries in cassandra

--- server/models/cassandra/index.js
+++ server/models/cassandra/index.js
@@ -11,7 +11,6 @@ client.connect()
 const insertProduct = (id, productItem, pictureUrls, liked, callback) => {
   const query = "INSERT INTO btetsy.product (id, product_item, picture_url, liked) VALUES (?, ?, ?,  ?)";
 
-  //spread maybe?
   client.execute(query, [id, productItem, pictureUrls, liked], {prepare: true})
     .then(result => console.log("Row updated"))
     .catch(err => console.error(err));
@@ -19,9 +18,17 @@ const insertProduct = (id, productItem, pictureUrls, liked, callback) => {
 
 const getProductById = (id, callback) => {
   client.execute('SELECT * FROM btetsy.product WHERE id=?', [id], {prepare: true})
-  .then(result => callback(result.rows[0]))
+  .then(result => console.log(result.rows[0]))
   .catch(err => console.error(err));
 }
+
+const copyCSV = (path)  => {
+  client.execute(`COPY btetsy.product (id, product_item, picture_url, liked) FROM ${path} WITH HEADER=TRUE;`)
+  .then(result => console.log("Copy succeeded"))
+  .catch(err => console.error(err));
+}
+
+copyCSV(__dirname + '/../../seed/cassandra/product_cassandra.csv');
 
 module.exports = {
   getProductById,
--- server/seed/cassandra/generateProductCSV.js
+++ server/seed/cassandra/generateProductCSV.js
@@ -11,20 +11,34 @@ const random = (min, max) => {
 (() => {
   writer.pipe(fs.createWriteStream(__dirname + '/product_cassandra.csv'));
   let i=10000000;
-
+  //{'element', 'otherElement', 'another'}
   function writeProducts() {
     var ok = true;
+
     do {
+      let build = '';
+      const randomNum = random(0, 5);
+      for(let i=0; i<randomNum; i++){
+        if(i !== randomNum-1){
+          build += "'" + faker.image.image() + "'" + ",";
+        }
+        else{
+          build += "'" + faker.image.image() + "'";
+        }
+      }
+      images = `{${build}}`;
+
       i -= 1;
       if (i === 0) {
         // last time!
-        writer.write({id: 10000000-i, product_item: faker.commerce.productName(), liked: false});
+        writer.write({id: 10000000-i, product_item: faker.commerce.productName(), picture_url: images, liked: false});
         console.log('finished images');
+        console.timeEnd("Whatever");
         writer.end();
       } else {
         // see if we should continue, or wait
         // don't pass the callback, because we're not done yet.
-        ok = writer.write({id: 10000000-i, product_item: faker.commerce.productName(), liked: false});
+        ok = writer.write({id: 10000000-i, product_item: faker.commerce.productName(), picture_url: images, liked: false});
       }
     } while (i > 0 && ok);
     if (i > 0) {
@@ -34,5 +48,7 @@ const random = (min, max) => {
     }
   }
 
+  console.time("Whatever");
   writeProducts();
+
 })();
