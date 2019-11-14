cc577084d58c0f3e0a0b70e932d8b1f0f94465f1
Brian Hambleton <bhambl3t0n1@gmail.com>
2019-11-13 18:06:44 -0800

    Changed path for models in main server file

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3003;
 const parser = require('body-parser');
-const models = require('../models/postgres/index.js');
+const models = require('./models/index.js');
 
 app.use(express.static(__dirname + '/../../dist'));
 app.use(parser.json());
